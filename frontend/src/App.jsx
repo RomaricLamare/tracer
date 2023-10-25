3ff9ff31ab8271065cc662bcf2f4c3d6cf2e1638
RomaricLamare <romariclamare@gmail.com>
2023-10-25 10:14:15 +0200

    graph s'affiche sur accueil site joomla

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,26 @@
 import "./App.css";
+// import { useNavigate } from "react-router-dom";
 import Router from "./navigation/Router";
 
 function App() {
+  // const navigate = useNavigate();
+
+  /* const navigateToGraph = () => {
+    navigate("/");
+  };
+
+  const navigateToTest = () => {
+    navigate("/test");
+  }; */
+
   return (
     <div className="App">
+      {/* <button type="button" onClick={navigateToGraph}>
+        Toggle Graph
+      </button>
+      <button type="button" onClick={navigateToTest}>
+        TEST
+  </button> */}
       <main>
         <Router />
       </main>
--- frontend/src/navigation/Router.jsx
+++ frontend/src/navigation/Router.jsx
@@ -3,12 +3,16 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { ReactFlowProvider } from "reactflow";
 import Graph from "../pages/Graph";
+// import Test from "../pages/Test";
 
 function Router() {
+  console.warn("Coucou");
+
   return (
     <ReactFlowProvider>
       <Routes>
         <Route path="/" element={<Graph />} />
+        {/* // <Route path="/test" element={<Test />} /> */}
       </Routes>
     </ReactFlowProvider>
   );
